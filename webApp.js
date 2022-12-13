cfa213022ad7d364958b32944d53fb56f786d4a8
ethan <gohho1008@gmail.com>
2022-12-13 18:49:53 +0900

    test commit

---
+++ webApp.js
@@ -0,0 +1,15 @@
+
+const data = {
+    userName : "",
+    userAge : "",
+    userSex : ""
+};
+
+
+const main = () => {
+    //get input data into dataStruct
+    const input = prompt("What's your name?");
+    alert(`Your name is ${input}`);
+}
+
+main();
